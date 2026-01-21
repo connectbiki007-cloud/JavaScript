75483142c6b693cef87aac67d4f831776d84ecb5
Biki079 <bikashkc2057@gmail.com>
2026-01-21 21:18:26 +0545

    Operator & Question

--- Day 2/app.js
+++ Day 2/app.js
@@ -11,15 +11,68 @@
 // console.log(b**2);
 
 // Assignment Operators in JavaScript
-let a = 10
-a += 5
-console.log(a);
-a -= 3
-console.log(a);
-a *=2
-console.log(a);
-a /= 5
-console.log(a);
-a %= 4
-console.log(a);
+// let a = 10
+// a += 5
+// console.log(a);
+// a -= 3
+// console.log(a);
+// a *=2
+// console.log(a);
+// a /= 5
+// console.log(a);
+// a %= 4
+// console.log(a);
  
+
+// Comparison Operators in JavaScript
+let x = 10
+let y = 20
+console.log(x==y);
+console.log(x=="10"); // compare number with string number
+console.log(x==="10"); // compare number with string number and also check data type
+console.log(20 != x); // not equal
+console.log(x!==y); // not equal and also check data type
+console.log(x>y);
+console.log(x>=y);
+
+// pratice questions
+console.log((60>50) + 5);
+
+let a = 10
+let b = 20
+let c = 5
+console.log(a<b<c);
+
+// Comparison Operators in JavaScript
+let m = 3
+let n = 5
+console.log(m<10 && n<20);
+console.log(10 == 20 || 20<30);
+console.log(! true);
+
+// String Operators in JavaScript
+let firstName = "Bikash"
+let lastName = "KC"
+console.log(firstName+lastName);
+console.log(firstName +="!");
+
+// Ternary Operator in JavaScript
+let age = 20
+let canVote = (age > 18) ? "Yes you can vote" : "No you can't vote"
+console.log(canVote);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
